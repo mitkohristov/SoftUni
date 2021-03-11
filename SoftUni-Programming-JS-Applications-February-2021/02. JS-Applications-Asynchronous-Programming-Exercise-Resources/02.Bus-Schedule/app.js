347d2d6ec53340296b541e7c681772e2f9344a2e
Mitko Hristov <mitko.hristov@gmail.com>
2021-03-11 23:35:42 +0200

    Bus Schedule app.js updated

--- SoftUni-Programming-JS-Applications-February-2021/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/02.Bus-Schedule/app.js
+++ SoftUni-Programming-JS-Applications-February-2021/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/02.Bus-Schedule/app.js
@@ -1,11 +1,52 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
+    const departBtn = document.getElementById('depart')
+    const arriveBtn = document.getElementById('arrive')
+    const info = document.getElementById('info').getElementsByClassName('info')
+
+
+    depot = {
+        name: '',
+        next: "depot"
+
     }
 
-    function arrive() {
-        console.log('Arrive TODO...');
+    async function depart() {
+      
+        const id = depot.next
+        const url = 'http://localhost:3030/jsonstore/bus/schedule/' + id
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log(depot)
+       
+            depot.name=  data.name
+            depot.next = data.next
+        
+            console.log(depot)
+        info[0].textContent = `Next stop ${depot.name}`
+      
+        departBtn.disabled = true
+        arriveBtn.disabled = false
+
+    }
+
+     async function arrive() {
+        info[0].textContent = `Arriving at ${depot.name}`
+        const id = depot.next
+        const url = 'http://localhost:3030/jsonstore/bus/schedule/' + id
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log(data)
+        
+            depot.name = data.name
+            depot.next =  data.next
+        
+       
+   console.log(depot)
+        
+        arriveBtn.disabled = true
+        departBtn.disabled = false
+        
     }
 
     return {
