470178920dc86ddccf268a33393f16bffe7f79c2
Mitko Hristov <mitko.hristov@gmail.com>
2021-03-11 22:00:16 +0200

    Updated app.js

--- SoftUni-Programming-JS-Applications-February-2021/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
+++ SoftUni-Programming-JS-Applications-February-2021/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
@@ -1,33 +1,34 @@
 async function getInfo() {
-const id = document.getElementById('stopId').value;
-buses.innerHTML = ''
+    const id = document.getElementById('stopId').value;
+    buses.innerHTML = ''
+    
+    try {
+        document.getElementById('stopId').value = ''
+        const url = 'http://localhost:3030/jsonstore/bus/businfo/' + id
+        const response = await fetch(url)
+        if (!response.ok) {
+            throw new Error(response.message)
+        }
+        const data = await response.json()
+        const stopName = document.getElementById('stopName')
+        const buses = document.getElementById('buses')
+        stopName.textContent = data.name
 
-try{
-    const url =  'http://localhost:3030/jsonstore/bus/businfo/' + id
-    const response = await fetch(url)
-    if(!response.ok){
-        throw new Error(response.message)
+
+        Object.keys(data.buses).forEach(b => {
+
+            const liElement = document.createElement('li')
+            liElement.textContent = `Bus ${b} arrives in ${data.buses[b]} minutes`
+            buses.appendChild(liElement)
+
+
+        })
+
+
+    } catch (err) {
+        stopName.textContent = "Error"
+
     }
-    const data = await response.json()
-    const stopName = document.getElementById('stopName')
-    const buses= document.getElementById('buses')
-    stopName.textContent = data.name
-    
-    
-    Object.keys(data.buses).forEach(b =>{
-            
-        const liElement = document.createElement('li')
-      liElement.textContent  =  `Bus ${b} arrives in ${data.buses[b]} minutes`
-      buses.appendChild(liElement)
-     
-    
-    })
-    
-
-}catch (err) {
-    stopName.textContent = "Error"
-
-}
 
 
 
