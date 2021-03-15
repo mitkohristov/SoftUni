f95d08537dc34d1e5a08ba6963a17d4c5049b986
Mitko Hristov <mitko.hristov@gmail.com>
2021-03-15 13:08:06 +0200

    apllied logic to app.js

--- SoftUni-Programming-JS-Applications-February-2021/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/06.Blog/app.js
+++ SoftUni-Programming-JS-Applications-February-2021/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/06.Blog/app.js
@@ -1,5 +1,68 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('btnLoadPosts').addEventListener('click', loadPosts)
+    document.getElementById('btnViewPost').addEventListener('click', getAll)
+    const posts = document.getElementsById('posts')
+
+
+     
+
+
 }
+ function getAll(){
+    const postid = posts.value
+    loadComments(postid)
+    loadPostDetails(postid)
+}
+async function loadComments(postid) {
+    const  commentArea = document.getElementById('post-comments')
+    commentArea.innerHTML = ''
+    const postComments = 'http://localhost:3030/jsonstore/blog/comments/'
+    // const postDetails = 'http://localhost:3030/jsonstore/blog/posts'
+    const response = await fetch(postComments)
+    const data = await response.json()
+     const result = Object.values(data).filter(c =>c.postId == postid)
+     result.forEach(c =>{
+        const liElement = document.createElement('li')
+        liElement.textContent = c.text
+        commentArea.appendChild(liElement)
+     })
+     
+    
+    
+}
+async function loadPostDetails(postid) {
+    const postTitle = document.getElementById('post-title')
+    const postBody = document.getElementById('post-body')
+    postBody.innerHTML = ''
+    postTitle.innerHTML = ''
+    const url = 'http://localhost:3030/jsonstore/blog/posts/' +postid
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
+     postTitle.textContent = data.title
+     const liElement = document.createElement('li')
+     liElement.textContent =data.body
+     postBody.appendChild(liElement)
+     
+  
+}
+
+
+async function loadPosts() {
+    const url = 'http://localhost:3030/jsonstore/blog/posts'
+    const response = await fetch(url)
+    const data = await response.json()
+
+    Object.entries(data).forEach(o => {
+        const res = document.createElement('option')
+        res.value = o[0]
+        res.innerText = o[1].title
+        posts.appendChild((res))
+    })
+
+}
+
+
+
 
 attachEvents();
