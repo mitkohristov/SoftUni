35cd87e5e44b549850967a61a4e719e6e89ad451
Mitko Hristov <mitko.hristov@gmail.com>
2021-03-22 11:03:30 +0200

    logic for app js

--- SoftUni-Programming-JS-Applications-February-2021/03. JS-Applications-Data-and-Authentication-Exercise-Resources/01.Messenger/app.js
+++ SoftUni-Programming-JS-Applications-February-2021/03. JS-Applications-Data-and-Authentication-Exercise-Resources/01.Messenger/app.js
@@ -1,5 +1,51 @@
 function attachEvents() {
-    console.log('TODO...');
+    const refreshBtn = document.getElementById('refresh').addEventListener('click', e =>{
+        e.preventDefault()
+        
+         getData()
+    })
+    
+    const sumbitBtn = document.getElementById('submit').addEventListener('click',e =>{
+        e.preventDefault();
+    const author = document.getElementById('author').value
+    const content = document.getElementById('content').value
+
+    sendData({author,content})
+       
+        getData()
+    })
 }
 
 attachEvents();
+
+
+async function sendData(d) {
+
+    const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(d)
+    }
+     const response = await fetch('http://localhost:3030/jsonstore/messenger',options)
+     const data = await response.json()
+      return await response.json()
+    
+    
+    
+    }
+
+    async function getData(){
+        const textArea = document.getElementById('messages')
+        textArea.value = ''
+        const options = {
+            method: 'get',
+            headers: { 'Content-Type': 'application/json' }
+           
+        }
+        const responce = await fetch('http://localhost:3030/jsonstore/messenger',options)
+        const data = await responce.json()
+      
+        Object.values(data).forEach(o =>{
+            textArea.value += `${o.author}: ${o.content}\n`
+        })
+    }
