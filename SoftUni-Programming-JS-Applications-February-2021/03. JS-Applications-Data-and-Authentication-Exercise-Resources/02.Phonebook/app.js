1554f89fc1498d69f379e8529447ff759c641e87
Mitko Hristov <mitko.hristov@gmail.com>
2021-03-22 11:24:04 +0200

    updated

--- SoftUni-Programming-JS-Applications-February-2021/03. JS-Applications-Data-and-Authentication-Exercise-Resources/02.Phonebook/app.js
+++ SoftUni-Programming-JS-Applications-February-2021/03. JS-Applications-Data-and-Authentication-Exercise-Resources/02.Phonebook/app.js
@@ -1,5 +1,31 @@
 function attachEvents() {
-    console.log('TODO...');
+ const loadBtn = document.getElementById('btnLoad').addEventListener('click', e=> {
+ e.preventDefault()
+ getData()
+
+ })
+
+  
 }
 
 attachEvents();
+
+async function getData(){
+    const ul = document.getElementById('phonebook')
+    ul.innerHTML = ''
+    const options = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    }
+
+    const response = await fetch('http://localhost:3030/jsonstore/phonebook',options)
+    const data = await response.json()
+   Object.values(data).forEach(o =>{
+    const li = document.createElement('li')
+    li.id = o._id
+     li.textContent = `${o.person}: ${o.phone}`
+     li.classList.add('button')
+
+     ul.appendChild(li)
+   })
+}
