1c91c00b934b2524b968ebc78f2bcf50a4e12262
Mitko Hristov <mitko.hristov@gmail.com>
2021-03-29 18:45:40 +0300

    updated

--- SoftUni-Programming-JS-Applications-February-2021/04.JS-Applications-Single Page Applications-Lab-Resources/05.Cookbook - transformed to SPA/src/app.js
+++ SoftUni-Programming-JS-Applications-February-2021/04.JS-Applications-Single Page Applications-Lab-Resources/05.Cookbook - transformed to SPA/src/app.js
@@ -1,3 +1,4 @@
+import {e} from './dom.js'
 async function getRecipeList() {
 
     const url = 'http://localhost:3030/data/recipes'
@@ -78,30 +79,7 @@ window.addEventListener('load', () => {
 
 })
 
-function e(type, attributes, ...content) {
-    const result = document.createElement(type);
 
-    for (let [attr, value] of Object.entries(attributes || {})) {
-        if (attr.substring(0, 2) == 'on') {
-            result.addEventListener(attr.substring(2).toLocaleLowerCase(), value)
-        } else {
-            result[attr] = value
-        }
-
-    }
-    content = content.reduce((a, c) => a.concat(Array.isArray(c) ? c : [c]), [])
-    content.forEach(e => {
-        if (typeof e == 'string' || typeof e == 'number') {
-            const node = document.createTextNode(e)
-            result.appendChild(node)
-        }else{
-            result.appendChild(e)
-        }
-    })
-
-    return result
-
-}
 
 window.addEventListener('load',() =>{
    if (sessionStorage.getItem('authToken') == null){
