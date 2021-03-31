5ec5ed08b26906b5354723f05e423c61523d0f10
Mitko Hristov <mitko.hristov@gmail.com>
2021-03-31 23:35:18 +0300

    updated

---
+++ SoftUni-Programming-JS-Applications-February-2021/04.JS-Applications-Single Page Applications-Lab-Resources/05.Cookbook - spa with edit and delete/src/details.js
@@ -0,0 +1,14 @@
+let main
+let section
+let setActiveNav
+
+export function setupDetails(mainTarget,sectionTarget,setActiveNavCb){
+main = mainTarget
+section = sectionTarget
+setActiveNav = setActiveNavCb
+}
+
+
+export async function showDetails(){
+     
+}
