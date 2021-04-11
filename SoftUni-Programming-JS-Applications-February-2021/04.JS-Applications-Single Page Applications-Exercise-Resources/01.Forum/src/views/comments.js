c656289eff325f7343e381bf785e1a30f31407eb
Mitko Hristov <mitko.hristov@gmail.com>
2021-04-11 13:32:11 +0300

    updated

--- SoftUni-Programming-JS-Applications-February-2021/04.JS-Applications-Single Page Applications-Exercise-Resources/01.Forum/src/views/comments.js
+++ SoftUni-Programming-JS-Applications-February-2021/04.JS-Applications-Single Page Applications-Exercise-Resources/01.Forum/src/views/comments.js
@@ -1,55 +1,42 @@
-async function postDetails(id){
-    const response  =await fetch('http://localhost:3030/jsonstore/collections/myboard/posts/' + id);
+async function postDetails(id) {
+    const response = await fetch('http://localhost:3030/jsonstore/collections/myboard/posts/' + id);
     const data = await response.json();
     return data
 }
 
-function createCommentsPreview(comment){
-  const div = document.createElement('div');
-  div.classList.add('comment')
+function createCommentsPreview(comment) {
+    const div = document.createElement('div');
+    div.classList.add('comment')
 
-    div.innerHTML =  `<div class="header">
+    div.innerHTML = `<div class="header">
                     <img src="./static/profile.png" alt="avatar">
                     <p><span>${comment.username}</span> posted on <time>2020-10-10 12:08:28</time></p>
             
                     <p class="post-content">${comment.text}</p>
                 </div>
             </div>`
-         
-            return div
+
+    return div
 
 }
 
-let main ;
+let main;
 let section;
 
 
-export function setupCommentsSection(mainTarget,sectionTarget){
-
+export function setupCommentsSection(mainTarget, sectionTarget) {
     main = mainTarget;
     section = sectionTarget;
-  
+
 }
 
 
 
-export async function showCommentsSection(id){
-  main.innerHTML = '';
-  main.appendChild(section);
-
-  const result = await postDetails(id)
-  
-
+export async function showCommentsSection(id) {
+    main.innerHTML = '';
+    main.appendChild(section);
+    const result = await postDetails(id)
     const res = createCommentsPreview(result)
-    
-   
-
     main.appendChild(res);
-     
-
-
- 
-    
-
 }
 
